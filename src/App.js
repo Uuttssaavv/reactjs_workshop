52c434c5e3e36fa0f4d903a25a7988487a69ff68
Uuttssaavv <ghimireutsav11@gmail.com>
2022-05-04 12:26:44 +0545

    react router pagination

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import "./App.css";
 import Home from "./components/home";
 import Error404 from "./components/404";
 import Page1 from "./components/page1";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <p>Ecom App</p>
-      </header>
+      </header> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/page1" element={<Page1 />} />
+          <Route path="*" element={<Error404 />} />
+          <Route />
+          <Route />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
