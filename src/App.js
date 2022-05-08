a775d6577ed56f891b58e57d4b5ed1985cde0341
Uuttssaavv <ghimireutsav11@gmail.com>
2022-05-08 07:22:30 +0545

    imported parent to root

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import "./App.css";
-import Home from "./components/home";
-import Error404 from "./components/404";
-import Page1 from "./components/page1";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Home from "./components/home";
+// import Error404 from "./components/404";
+// import Page1 from "./components/page1";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Parent from "./components/parent";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/page1" element={<Page1 />} />
-          <Route path="*" element={<Error404 />} />
-          <Route />
-          <Route />
-        </Routes>
-      </BrowserRouter>
+      <Parent />
     </div>
   );
 }
