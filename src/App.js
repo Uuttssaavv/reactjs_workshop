5cd38dc1539f6375b476ef2cf96e6d2fa3b86ea5
Uuttssaavv <ghimireutsav11@gmail.com>
2022-05-08 07:14:43 +0545

    day 3 started

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <p>Ecom App</p>
-      </header> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
