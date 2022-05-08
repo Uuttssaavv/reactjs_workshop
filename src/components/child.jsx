c94deb42e3b2d74cc72ec168801fad71bc704bea
Uuttssaavv <ghimireutsav11@gmail.com>
2022-05-08 07:21:00 +0545

    imported grandchild in child

--- src/components/child.jsx
+++ src/components/child.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import GrandChild from "./grandchild";
 export default function Child(){
     return (
         <div>
         <h1>I am Child</h1>
+        <GrandChild/>
         </div>
     );
 }
