36d674b8203f038d163dc7aebca46cde28fc695c
Uuttssaavv <ghimireutsav11@gmail.com>
2022-05-08 09:30:59 +0545

    fetched product lists

--- src/components/grandchild.jsx
+++ src/components/grandchild.jsx
@@ -1,10 +1,36 @@
-import React,{ useContext } from "react";
+import React,{ useContext,useState, useEffect } from "react";
 import { AppContext } from "../appContext";
+import axios from 'axios';
 export default function GrandChild(){
     const {AppName}=useContext(AppContext);
+   const [productList,setProductList]= useState([]);    
+    useEffect(()=>{
+    getProductList();
+    },[]);
+    const getProductList=async()=>{
+      const response=await  axios.get('https://dummyjson.com/products');
+    //     // handle success
+       setProductList(response['data']['products']);
+    // console.log(response['data']['products']);
+    // console.log(response);
+     
+    }
     return (
         <div>
         <h1>I am GrandChild: {AppName}</h1>
+        <span>Peoduct list</span><br/>
+       {
+           productList.map((product)=>{
+              return (
+                <>
+                <img src={product.images[0]} height={'100px'} width={'100px'} alt="images"/><br/>
+                <span>Name: {product.title}</span><br/>
+                <span>Price: {product.price}</span><br/>
+                </>
+                );
+               
+           })
+       }
         </div>
     );
 }
