8a33a9a0687aceec9c58ba7d15edc77e434b0bbb
Uuttssaavv <ghimireutsav11@gmail.com>
2022-05-10 07:46:49 +0545

    created login component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 // import Home from "./components/home";
-// import Error404 from "./components/404";
+import Error404 from "./components/404";
 // import Page1 from "./components/page1";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GrandChild from "./components/grandchild";
@@ -14,6 +14,7 @@ function App() {
           <Route path="/products" element={<GrandChild />} />
           <Route path="/product/:id" element={<ProductDetails />} />
           <Route path="/" element={<GrandChild />} />
+          <Route path="*" element={<Error404 />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/login.jsx
@@ -0,0 +1,9 @@
+export default function LoginPage(){
+    return <div>
+        <form>
+            <input type="text" placeholder="User Name" />
+            <input type="text" placeholder="Password" />
+            <button> Login</button>
+        </form>
+    </div>
+}
